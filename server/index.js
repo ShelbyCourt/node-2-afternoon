0582308df6ca886a4bcbb879ccde6adce030e5a8
ShelbyCourt <shelby.b.courtney@gmail.com>
2020-05-19 15:46:27 -0700

    Completed

--- server/index.js
+++ server/index.js
@@ -1,18 +1,21 @@
-const express = require('express');
-const app = express();
-const mc = require('.controllers/messages_controller');
+const express = require("express");
+const mc = require("./controllers/messages_controller");
 const SERVER_PORT = 3001;
 
+const app = express();
+
 app.use(express.json());
+app.use(express.static(__dirname + '/../public/build'));
 
 const messagesBaseUrl = "/api/messages";
 app.post(messagesBaseUrl, mc.create);
 app.get(messagesBaseUrl, mc.read);
-app.put(messagesBaseUrl, mc.update);
-app.delete(messagesBaseUrl, mc.delete);
-
+app.put(`${messagesBaseUrl}/:id`, mc.update);
+app.delete(`${messagesBaseUrl}/:id`, mc.delete);
 
 app.listen(SERVER_PORT, () => {
     console.log(`Server listening on ${SERVER_PORT}`)
 });
+
+
 
